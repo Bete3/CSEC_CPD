42a27a46d48e6f700973bfa15aa85af6b7f9d876
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2025-02-06 05:14:32 +0300

    Create Word.py

---
+++ Word.py
@@ -0,0 +1,9 @@
+def solve():
+    s = input().strip()
+    uppercase_count = sum(1 for c in s if c.isupper())
+    lowercase_count = len(s) - uppercase_count  
+    if uppercase_count > lowercase_count:
+        print(s.upper())
+    else:
+        print(s.lower())
+solve ()
